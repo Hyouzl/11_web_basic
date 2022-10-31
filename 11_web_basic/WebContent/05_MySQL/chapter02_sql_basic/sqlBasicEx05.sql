76452f7b5849cd04ac9f1d84e6a03c5fe94ebf49
19_web_hyr <19_web_hyr@302-15>
2022-10-31 21:45:19 +0900

    RER

---
+++ 11_web_basic/WebContent/05_MySQL/chapter02_sql_basic/sqlBasicEx05.sql
@@ -0,0 +1,36 @@
+/*
+
+	# 데이터 조작어 (DML : Data Manipulation Language) 
+	
+		[ DELETE문 ]
+	
+		- 테이블의 레코드를 삭제한다.
+		- 대부분 where절과 같이 사용한다.
+	
+	   [ 레코드 삭제 형식 ]
+	   
+	    - DELETE FROM TABLE WHERE FIELD_CONDITION='DATA';
+	    
+		EX) 
+			DELETE FROM 
+						MEMBER 
+			WHERE 
+						NO=1;
+
+*/
+
+# 상품코드가 'P10006'인 레코드 삭제
+
+
+# 상품코드가 'P10005'인 레코드 삭제
+
+
+# 상품코드가 'P10004'인 레코드 삭제
+
+
+
+		
+# 전체 데이터 삭제 예시
+
+
+	
